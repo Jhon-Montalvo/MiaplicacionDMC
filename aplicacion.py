7791e709a1d54858ee99b5e44a3fd0f358ce73cc
Jhon-Montalvo <jhonkmontalvo@gmail.com>
2026-04-26 20:51:58 -0500

    Add files via upload

---
+++ aplicacion.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import numpy as np
+import libreria_funciones as lf
+
+st.title("Mi primera app")
+
+st.sidebar.title("Datos")
+st.image("Logopy.png", width=100)
+st.sidebar.image("logo.png")
+st.title("Clase 5 Funciones")
+
+p=st.number_input("Ingrese el monto Principal")
+t=st.number_input("Ingrese la tasa anual")
+a=st.slider("Seleccione el numero de años de prestamo",min_value=1, max_value=5)
+pa=st.number_input("Cantindad de pago por año")
+
+cota=lf.cuota_prestamo(p,t,a,pa)*2
+st.write(cota)
